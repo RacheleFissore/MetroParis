9af7ac3e356821c21ba60c81593ca7f7991b56fb
Rachi <Rachi@192.168.1.7>
2022-05-16 21:24:02 +0200

    Versione prof

--- src/main/java/it/polito/tdp/metroparis/MetroController.java
+++ src/main/java/it/polito/tdp/metroparis/MetroController.java
@@ -1,5 +1,78 @@
 package it.polito.tdp.metroparis;
 
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.metroparis.model.Fermata;
+import it.polito.tdp.metroparis.model.Model;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class MetroController {
-	
+
+	private Model model;
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<Fermata> cmbArrivo;
+
+    @FXML
+    private ComboBox<Fermata> cmbPartenza;
+    
+    @FXML
+    private TableColumn<Fermata, String> colonnaFermata; // Il primo generic della colonna è sempre uguale a quello della riga (Fermata), il secondo invece è il valore del contenuto
+    										  // della colonna nella tabella
+
+    @FXML
+    private TableView<Fermata> tablePercorso; // E' il tipo di oggetto che viene rappresentato nella riga
+
+    @FXML
+    private TextArea txtRisultato;
+
+    @FXML
+    void handleCerca(ActionEvent event) {
+    	Fermata partenza = cmbPartenza.getValue();
+    	Fermata arrivo = cmbArrivo.getValue();
+    	
+    	if(partenza != null && arrivo != null && !partenza.equals(arrivo)) {
+    		List<Fermata> percorso = model.calcolaPercorso(partenza, arrivo);
+    		tablePercorso.setItems(FXCollections.observableArrayList(percorso));
+    		txtRisultato.setText("Percorso trovato con " + percorso.size() + " stazioni");
+    	}
+    	else {
+    		txtRisultato.setText("Devi selezionare due stazioni diverse tra loro\n");
+    	}
+     }
+
+    @FXML
+    void initialize() {
+        assert cmbArrivo != null : "fx:id=\"cmbArrivo\" was not injected: check your FXML file 'Metro.fxml'.";
+        assert cmbPartenza != null : "fx:id=\"cmbPartenza\" was not injected: check your FXML file 'Metro.fxml'.";
+        assert txtRisultato != null : "fx:id=\"txtRisultato\" was not injected: check your FXML file 'Metro.fxml'.";
+
+        // Questo lavoro devo farlo per ogni colonna della tabella
+        colonnaFermata.setCellValueFactory(new PropertyValueFactory<Fermata, String>("nome")); // COstruisce qualsiasi cosa ci stia dentro una cella
+        														// Metto i generics della tabella e tra () il nome dell'attributo di Fermata che voglio rappresentare
+    }
+
+	public void setModel(Model m) {
+		model = m;
+		List<Fermata> fermate = model.getFermate();
+		cmbPartenza.getItems().addAll(fermate); // getItems() è l'elenco degli elementi della tendina
+		cmbArrivo.getItems().addAll(fermate);
+	}
+
 }
+
+
--- src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
+++ src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
@@ -7,9 +7,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.management.RuntimeErrorException;
+
 import com.javadocmd.simplelatlng.LatLng;
 
 import it.polito.tdp.metroparis.model.Connessione;
+import it.polito.tdp.metroparis.model.CoppiaId;
 import it.polito.tdp.metroparis.model.Fermata;
 import it.polito.tdp.metroparis.model.Linea;
 
@@ -69,6 +72,106 @@ public class MetroDAO {
 		return linee;
 	}
 
-	
+	public boolean isFermataConnesse(Fermata partenza, Fermata arrivo) {
+		String sqlString = "SELECT COUNT(*) AS cnt "
+				+ "FROM connessione "
+				+ "WHERE id_stazP = ? "
+				+ "AND id_stazA = ?";
+		
+		Connection connection = DBConnect.getConnection();
+		
+		try {
+			PreparedStatement statement = connection.prepareStatement(sqlString);
+			statement.setInt(1, partenza.getIdFermata());
+			statement.setInt(2, arrivo.getIdFermata());
+			ResultSet resultSet = statement.executeQuery();
+			resultSet.first(); // Ho solo un elemento, quindi mi posiziono sul primo
+			int count = resultSet.getInt("cnt");
+			connection.close();
+			return count > 0; // Ritorna true solo le due partenze sono connesse tra loro, cioè se il cnt > 0 vuol dire che c'è una connessione
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("Errore db", e);
+		}
+	}
 
+	public List<Integer> getIdFermateConnesse(Fermata partenza) {
+		String sqlString = "SELECT id_stazA "
+				+ "FROM connessione "
+				+ "WHERE id_stazP = ? "
+				+ "GROUP BY id_stazA";
+		
+		Connection connection = DBConnect.getConnection();
+		
+		try {
+			PreparedStatement statement = connection.prepareStatement(sqlString);
+			statement.setInt(1, partenza.getIdFermata());
+			ResultSet resultSet = statement.executeQuery();
+			List<Integer> results = new ArrayList<Integer>();
+			
+			while (resultSet.next()) {
+				results.add(resultSet.getInt("id_stazA"));				
+			}
+			connection.close();
+			return results; 
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	public List<Fermata> getFermateConnesse(Fermata partenza) { // Mi restitiuisce una lista di oggetti della classe Fermata
+		final String sqlString = "SELECT id_fermata, nome, coordx, coordy "
+				+ "FROM fermata "
+				+ "WHERE id_fermata IN ( SELECT id_stazA "
+				+ "							FROM connessione "
+				+ "							WHERE id_stazP = ? "
+				+ "							GROUP BY id_stazA ) "
+				+ "ORDER BY nome ASC";
+		Connection connection = DBConnect.getConnection();
+		
+		try {
+			PreparedStatement statement = connection.prepareStatement(sqlString);
+			statement.setInt(1, partenza.getIdFermata());
+			ResultSet resultSet = statement.executeQuery();
+			List<Fermata> results = new ArrayList<Fermata>();
+			
+			while (resultSet.next()) {
+				Fermata f = new Fermata(resultSet.getInt("id_Fermata"), resultSet.getString("nome"),
+						new LatLng(resultSet.getDouble("coordx"), resultSet.getDouble("coordy")));
+				results.add(f);
+				
+			}
+			connection.close();
+			return results; 
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public List<CoppiaId> getAllFermateConnesse() {
+		String sqlString = "SELECT DISTINCT id_stazP, id_stazA "
+				+ "FROM connessione";
+		
+		Connection connection = DBConnect.getConnection();
+		
+		try {
+			PreparedStatement statement = connection.prepareStatement(sqlString);
+			ResultSet resultSet = statement.executeQuery();
+			List<CoppiaId> results = new ArrayList<CoppiaId>();
+			
+			while (resultSet.next()) {
+				CoppiaId coppiaId = new CoppiaId(resultSet.getInt("id_stazP"), resultSet.getInt("id_stazA"));
+				results.add(coppiaId);
+				
+			}
+			connection.close();
+			return results; 
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
